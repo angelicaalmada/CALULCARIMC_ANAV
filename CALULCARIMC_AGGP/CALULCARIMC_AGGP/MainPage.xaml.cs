66fd8bc25c191e4d6e50d1c59870d57f22ae0089
Angelica <Angelica@ANGELICA>
2023-10-25 17:06:24 -0700

    Add project files.

---
+++ CALULCARIMC_AGGP/CALULCARIMC_AGGP/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CALULCARIMC_AGGP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        //float peso;
+        //float altura;
+        float imc;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Peso.Text) && !string.IsNullOrEmpty(Altura.Text)) { 
+               
+                CalulcarIMC(float.Parse(Peso.Text), float.Parse(Altura.Text));
+
+                string rango = "";
+                if (imc < 18)
+                {
+                    rango = "Bajo peso";
+                }
+                else if (imc <= 24.9)
+                {
+                    rango = "Peso normal";
+                }
+
+                else if (imc > 24.9 && imc < 30)
+                {
+                    rango = "Sobrepeso";
+                }
+                else
+                {
+                    rango = "Obesidad";
+                }
+                DisplayAlert("Rango de peso", rango, "Salir");
+            }
+            else
+            {
+                DisplayAlert("Campos Vacíos", "Ingresa todos los datos", "Salir");
+            }
+        }
+      
+        private void CalulcarIMC(float p, float a)
+        {
+            imc = p / (a * a);
+        }
+
+
+    }
+}
